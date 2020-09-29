c37c50d534208fb9cc944483e0cd9f261c5080af
Logan Mayfield <jlmayfield.4@gmail.com>
2020-09-29 13:58:04 -0500

    daily update

--- source_compare.py
+++ source_compare.py
@@ -32,6 +32,7 @@ import cvdataviz as cvdv
 
 # 17187,17 <-- Warren County, IL
 warren = [17187]
+p = 16981
 
 
 
@@ -58,8 +59,8 @@ data_idph = pd.read_csv('ILDPH_Reports.csv',
 
 
 #%%
-start_date = pd.to_datetime('2020-09-16')
-end_date = pd.to_datetime('2020-09-23') + pd.Timedelta(1,unit='D')
+start_date = pd.to_datetime('2020-09-13')
+end_date = pd.to_datetime('2020-09-26') #+ pd.Timedelta(1,unit='D')
 
 full_tests_wchd = cvda.expandWCHDData(cvdp.prepwchd(reports_wchd))
 tests_wchd = full_tests_wchd.loc[:,17,17187].loc[start_date:end_date]
@@ -72,24 +73,75 @@ tests_usaf = tests_usaf[['New Positive']]
 
 tests_idph = data_idph[['New Positive','New Tests']].loc[start_date:end_date].astype(int)
 
+
 #%%
 
-days = tests_wchd.index
+tests_idph['New Negative'] = tests_idph['New Tests'] - tests_idph['New Positive']
+tests_wchd['New Negative'] = tests_wchd['New Tests'] - tests_wchd['New Positive']
+tests_idph['Positvity'] = tests_idph['New Positive']/tests_idph['New Tests']
+tests_wchd['Positvity'] = tests_wchd['New Positive']/tests_wchd['New Tests']
+
+tests_idph['source'] = 'IDPH'   
+tests_usaf['source'] = 'USAFacts'
+tests_wchd['source'] = 'WCHD'
+
+idph = tests_idph.reset_index().set_index(['date','source'])
+usaf = tests_usaf.reset_index().set_index(['date','source'])
+wchd = tests_wchd.reset_index().set_index(['date','source'])
+all_sources = pd.concat([wchd,idph,usaf])
+
+#%%
+
+wcVil = all_sources.loc[(slice(None),['WCHD','IDPH']),:]
+tots = wcVil.iloc[:,0:3].groupby(level=1).sum()
+tots['Positivity'] = tots['New Positive']/tots['New Tests']
+
+weeks = wcVil.iloc[:,:3].groupby([pd.Grouper(level=1),
+                       pd.Grouper(level=0,freq='W-SUN',
+                                  closed='left',label='left')]).sum()
+
+weeks['per 100k'] = weeks['New Positive'] * 100000 / p
+weeks['Positivity'] = weeks['New Positive'] / weeks['New Tests']
+
+
+
+
+#%%
+
+days = [ str(d.date()) for d in tests_wchd.index]
 fig = go.Figure(data=[
-    go.Bar(name='WCHD', x=days, y=tests_wchd['New Positive']),
-    go.Bar(name='USAFacts', x=days, y=tests_usaf['New Positive']),
-    go.Bar(name='IDPH', x=days, y=tests_idph['New Positive'])
-])
-# Change the bar mode
-fig.update_layout(barmode='group')
+    go.Bar(name='Positive',
+           x=[days,['WCHD']*len(days)],
+           y=tests_wchd['New Positive'],
+           marker_color='red'),
+    go.Bar(name='Negative',
+           x=[days,['WCHD']*len(days)],
+           y=tests_wchd['New Negative'],
+           marker_color='blue'),
+    go.Bar(name='Positive',
+           x=[days,['IDPH']*len(days)],
+           y=tests_idph['New Positive'],
+           marker_color='red',
+           showlegend=False),
+    go.Bar(name='Negative',
+           x=[days,['IDPH']*len(days)],
+           y=tests_idph['New Negative'],
+           marker_color='blue',
+           showlegend=False),
+    go.Bar(name='Positive',
+           x=[days,['USAFacts']*len(days)],
+           y=tests_usaf['New Positive'],
+           marker_color='red',
+           showlegend=False)    
+    ])
+
+fig.update_layout(barmode='stack')
+
+
 plot(fig,filename='graphics/temp.html')
 
-days = tests_wchd.index
-fig = go.Figure(data=[
-    go.Bar(name='WCHD', x=days, y=tests_wchd['New Tests']),    
-    go.Bar(name='IDPH', x=days, y=tests_idph['New Tests'])
-])
-# Change the bar mode
-fig.update_layout(barmode='group')
-plot(fig,filename='graphics/temp.html')
+
+
+
+
 
