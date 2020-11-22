1834d7c81aa7c4f3f6d41e1d080927bea4af3782
Logan Mayfield <jlmayfield.4@gmail.com>
2020-11-22 10:54:05 -0600

    Now with Forecasts!
    I've added forecasts.py which reads from a local
    copy of the projections done by Shaman's team at
    Columbia (https://github.com/shaman-lab/COVID-19Projection).
    The projections are based off of case data from usafacts.org
    
    The goal is to be able to do a set of projection graphs
    for any county.At the moment I can quickly pull county data
    from usafacts and graph the county totals. For warren county
    I can graph total projections and some day to day. These will
    be generalized in a similar fashion to the actual total
    graphs.

---
+++ forecasts.py
@@ -0,0 +1,269 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct  7 12:31:03 2020
+
+@author: jlmayfield
+"""
+
+import os
+import pandas as pd
+import numpy as np
+
+import plotly.express as px
+import plotly.graph_objects as go
+from plotly.offline import plot
+from plotly.subplots import make_subplots
+
+from urllib.request import urlopen
+import json
+with urlopen('https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json') as response:
+    counties = json.load(response)
+
+MB_TOKEN = open(".mapbox_token").read()
+
+import cvdataprep as cvdp
+import cvdataanalysis as cvda
+import cvdataviz as cvdv
+#%%
+
+population, actuals = cvdp.loadusafacts()
+actuals = cvdp.prepusafacts(actuals)
+
+#%%
+
+def getCountyActuals(fips,usafdata=actuals,population=population):
+    df = usafdata.loc[(slice(None),slice(None),fips),:]
+    df = cvda.expandUSFData(df, population)
+    statefips = df.index.get_level_values(1).unique()[0]
+    df = df.loc[(slice(None),statefips,fips)]
+    firstcase = df['Total Positive'].ne(0).idxmax()
+    firstsunday = firstcase - pd.Timedelta((firstcase.weekday()+1)%7,unit='D')
+    return df.loc[firstsunday:]
+    
+
+
+def countyTotalGraph(data,name):
+    ## total cases via usafacts.org
+    tot = data['Total Positive'].max()
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=data.index,
+                             y=data['Total Positive'],
+                             mode='lines',
+                             fill='tozeroy'))
+    fig.update_layout(yaxis={'range':(0,tot+50)},
+                      title="Total Cases in "+name)
+    fname = 'graphics/'+name.replace(' ','_')+'_usafacts_total.html'
+    plot(fig,filename=fname)
+
+#%%
+
+
+wcil_actual = getCountyActuals(17187)
+kcil_actual = getCountyActuals(17095)
+
+countyTotalGraph(wcil_actual, 'Warren County')
+countyTotalGraph(kcil_actual, 'Knox County')
+
+
+#%%
+
+
+reported = ['report_2.5','report_25','report_50','report_75','report_97.5']
+newtotal = ['total_2.5','total_25','total_50','total_75','total_97.5']
+
+def readLatest():
+    def readone(case):
+        df = pd.read_csv(dir+'Projection_'+case+'.csv',
+                     #index_col='fips',
+                     parse_dates=['Date'],
+                     dtype={'fips':'int64'})
+        df['scenario'] = case
+        return df        
+    dir = '../COVID-19Projection/LatestProjections/'
+    cases = ['low','mid','high','nochange']
+    projs = [readone(c) for c in cases]
+    projs = pd.concat(projs)
+    projs = projs.set_index(['fips','scenario','Date'])
+    return projs.sort_index()
+
+def readDataset(dates,scenes):
+    def readone(d):
+        projs = [None] * len(scenes)
+        for i in range(len(scenes)):
+            df = pd.read_csv(d+'Projection_'+scenes[i]+'.csv',
+                           parse_dates=['Date'], dtype={'fips':'int64'}) 
+            df['scenario'] = scenes[i]
+            projs[i] = df
+        projs = pd.concat(projs)
+        projs = projs.set_index(['fips','scenario','Date'])
+        return projs.sort_index()            
+    dir = '../COVID-19Projection/'
+    dirs = [dir+'Projection_'+m+'/' for m in dates]    
+    projs = [readone(d) for d in dirs]
+    idxs = [p.index for p in projs]
+    keeps_idx = [idxs[i].difference(idxs[i+1]) for i in range(0,len(idxs)-1)] +\
+        [idxs[-1]]
+    keeps = [projs[i].loc[keeps_idx[i]] for i in range(0,len(keeps_idx))]
+    return pd.concat(keeps).sort_index()
+
+
+#%%
+
+# Load Projections 
+
+# not currently used but fit within the current modeling 
+oldnames = ['October1','October4','October8',
+            'October11','October15']
+
+# 5_1xhold,5_2xhold
+prev_models = ['October18','October22','October25',
+               'October29','November1','November5','November8']            
+current_models = ['November12','November15','November19'] #5_1xbeta,5_2xbeta      
+cur = oldnames[-1]
+
+# Most recent models
+curr = readDataset(current_models,['5_1xbeta','5_2xbeta','season4','nochange'] )
+# previous set of models. Some are missing season4 ?
+prev = readDataset(prev_models, ['5_1xhold','5_2xhold','nochange'])   
+
+
+#%%
+
+# Total Cases with Projections
+
+wcil_curr = curr.loc[17187,'5_2xbeta']
+lowertotal = wcil_curr['report_2.5'].cumsum().rename('report_2.5_total')
+uppertotal = wcil_curr['report_97.5'].cumsum().rename('report_97.5_total')
+medtotal = wcil_curr['report_50'].cumsum().rename('report_50_total')
+wcil_curr = pd.concat([wcil_curr,lowertotal,uppertotal,medtotal],axis=1)
+
+start_proj = wcil_curr.index[0]
+tot_at_start = wcil_actual.loc[start_proj-pd.Timedelta(1,unit='D')]['Total Positive']
+proj_max_total = wcil_curr['report_97.5_total'][-1] + tot_at_start
+
+fig = go.Figure()
+fig.add_trace(go.Scatter(x=wcil_actual.index,
+                         y=wcil_actual['Total Positive'],
+                         mode='lines',
+                         fill=None,
+                         line_color=px.colors.sequential.Blues[5],
+                         name = 'Actual Case Totals',
+                         showlegend=False
+                         ))
+fig.add_trace(go.Scatter(x=wcil_curr.index,
+                         y=wcil_curr['report_2.5_total']+tot_at_start,
+                         mode='lines',
+                         fill=None,
+                         line_color=px.colors.sequential.Blues[3],
+                         showlegend=False
+                         ))
+fig.add_trace(go.Scatter(x=wcil_curr.index,
+                         y=wcil_curr['report_97.5_total']+tot_at_start,
+                         mode='lines',
+                         fill='tonexty',
+                         line_color=px.colors.sequential.Blues[3],
+                         showlegend=False                         
+                         ))
+fig.add_trace(go.Scatter(x=wcil_curr.index,
+                         y=wcil_curr['report_50_total']+tot_at_start,
+                         mode='lines',
+                         fill=None,
+                         line_color=px.colors.sequential.Blues[4],
+                         showlegend=False                         
+                         ))
+fig.update_layout(yaxis={'range':(0,proj_max_total+50)},
+                  title="Total Cases")
+plot(fig,filename='graphics/wc_total_projection.html')
+
+#%%
+
+
+# last day in actual data set
+end_actual = wcil_actual.index[-1]
+# where to cutoff projection data set (+2 weekes of projects)
+end_proj = end_actual + pd.to_timedelta(14,unit='D')
+
+# WC, IL projections
+wcil_proj = curr.loc[17187]
+wcil_5x2 = wcil_proj.loc['5_2xbeta']
+# older
+old = prev.loc[17187,'5_2xhold']
+keep = old.index.difference(wcil_5x2.index)
+wcil_5x2 = pd.concat([old.loc[keep],wcil_5x2])
+
+proj = wcil_5x2.loc[:end_proj]
+# actual reported numbers from usafacts
+fig = go.Figure()
+fig.add_trace(go.Scatter(x=proj.index,
+                         y=proj['report_2.5'],
+                         mode='lines',
+                         name='2.5 percentile',
+                         showlegend=False,
+                         fill=None,
+                         line_color=px.colors.sequential.Blues[3],
+                         opacity=0.25
+                         ))
+fig.add_trace(go.Scatter(x=proj.index,
+                         y=proj['report_25'],
+                         fill='tonexty',
+                         mode='lines',
+                         name='25th percentile',
+                         showlegend=False,
+                         line_color=px.colors.sequential.Blues[3],
+                         opacity=0.5
+                         ))
+fig.add_trace(go.Scatter(x=proj.index,
+                         y=proj['report_25'],
+                         fill=None,
+                         mode='lines',
+                         name='25th percentile',
+                         showlegend=False,
+                         line_color=px.colors.sequential.Blues[6]
+                         ))
+fig.add_trace(go.Scatter(x=proj.index,
+                         y=proj['report_75'],
+                         fill='tonexty',
+                         mode='lines',
+                         name='75th percentile',
+                         showlegend=False,
+                         line_color=px.colors.sequential.Blues[6],
+                         opacity=0.5
+                         ))
+fig.add_trace(go.Scatter(x=proj.index,
+                         y=proj['report_97.5'],
+                         fill='tonexty',
+                         mode='lines',
+                         name='97.5 percentile',
+                         showlegend=False,
+                         line_color=px.colors.sequential.Blues[3],
+                         opacity=0.25
+                         ))                         
+
+#fig.add_trace(go.Scatter(x=proj.index,
+#                         y=proj['report_50'],                         
+#                         mode='lines',
+#                         name='50th percentile',
+#                         line_color=px.colors.sequential.Blues[8],
+#                         showlegend=False
+#                         ))
+
+act = wcil_actual[-31:]
+fig.add_trace(go.Bar(x=act.index,
+                         y=act['New Positive'],
+                         name='New Cases',
+                         marker_color=px.colors.sequential.Blues[5],
+                         #mode='markers'
+                         ))
+fig.add_trace(go.Scatter(x=act.index,
+                         y=act['7 Day Avg New Positive'],
+                         name='7 Day Average',
+                         line_color=px.colors.sequential.Blues[8]))
+fig.update_layout(title="Number of Reported Cases per Day with Forecasts",
+                  yaxis_range=(0,40),
+                  xaxis_range=(act.index[0], proj.index[-1])
+                  )
+plot(fig,filename='graphics/wc_usafacts_daily.html')
+
+#%%
+
