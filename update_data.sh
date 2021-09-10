99d71ab860a2f1a46141753ef2abdb54177e5e94
Logan Mayfield <jlmayfield.4@gmail.com>
2021-09-10 12:33:34 -0500

    daily update 9-10

--- update_data.sh
+++ update_data.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # get the latest datasets from usafacts.org
-wget -N https://usafactsstatic.blob.core.windows.net/public/data/covid-19/covid_confirmed_usafacts.csv
-wget -N https://usafactsstatic.blob.core.windows.net/public/data/covid-19/covid_deaths_usafacts.csv
-wget -N https://usafactsstatic.blob.core.windows.net/public/data/covid-19/covid_county_population_usafacts.csv
+wget -N https://static.usafacts.org/public/data/covid-19/covid_confirmed_usafacts.csv
+wget -N https://static.usafacts.org/public/data/covid-19/covid_deaths_usafacts.csv
+wget -N https://static.usafacts.org/public/data/covid-19/covid_county_population_usafacts.csv
 wget -N https://static.usafacts.org/public/data/external/COVID19_CDC_Vaccination_CSV_Download.csv
 # pull my spreadsheets on Warren County Data
 wget "https://docs.google.com/spreadsheets/d/1KR9c7TJBioHAMScGKByp2rPARCrV0n-hdDjkW7vpgU8/export?format=csv&gid=891318752" -O "WCHD_Reports.csv"
